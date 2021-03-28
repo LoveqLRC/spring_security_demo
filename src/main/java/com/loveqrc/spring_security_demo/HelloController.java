58fbf2c581fed6b2f5185ad0e8ec940fc9738712
rc <64215432@qq.com>
2021-03-28 09:26:41 +0800

    Initial commit

---
+++ src/main/java/com/loveqrc/spring_security_demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.loveqrc.spring_security_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloSecurity() {
+        return "hello security";
+    }
+}
